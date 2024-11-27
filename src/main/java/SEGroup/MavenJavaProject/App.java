1ab254d864a629a6f2711d6d0a27c697ad304a30
SatwikaReddy-16 <satwika1216@gmail.com>
2024-11-27 11:12:20 +0530

    Update App.java

--- src/main/java/SEGroup/MavenJavaProject/App.java
+++ src/main/java/SEGroup/MavenJavaProject/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "22BD1A6655" );
+        System.out.println( "poll Demo" );
     }
 }
